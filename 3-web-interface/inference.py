27faeee4eb0e0776316dd2e744ff255e2f06dffb
AIT SIDI HOU Mohamed <aitsidihoumohamed@gmail.com>
2026-03-26 19:12:37 +0100

    fix transform

--- 3-web-interface/inference.py
+++ 3-web-interface/inference.py
@@ -41,6 +41,7 @@ NORMALIZE_TRANSFORM = T.Compose([
     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
+#add TRANSFORM for compatibility with Spark UDFs
 TRANSFORM = NORMALIZE_TRANSFORM
 
 
