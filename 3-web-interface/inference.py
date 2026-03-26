819c93099c14b79ec696fccd5eef2859af0fa17e
AIT SIDI HOU Mohamed <aitsidihoumohamed@gmail.com>
2026-03-26 19:10:11 +0100

    fix : TRANSFORM == NORMALIZE_TRANSFORM

--- 3-web-interface/inference.py
+++ 3-web-interface/inference.py
@@ -41,6 +41,8 @@ NORMALIZE_TRANSFORM = T.Compose([
     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
+TRANSFORM = NORMALIZE_TRANSFORM
+
 
 # ============================================================================
 # SINGLETON MODEL LOADER
